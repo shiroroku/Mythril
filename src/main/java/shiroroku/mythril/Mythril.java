132cee88901f4d36a8fc2b876e2e6a555209e29e
shiroroku(Elise) <empresselise@proton.me>
2024-03-10 21:17:42 -0700

    added mythril toolset

--- src/main/java/shiroroku/mythril/Mythril.java
+++ src/main/java/shiroroku/mythril/Mythril.java
@@ -1,30 +1,14 @@
 package shiroroku.mythril;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 import shiroroku.mythril.Registries.ItemRegistry;
 
@@ -33,6 +17,8 @@ public class Mythril {
 
     public static final String MODID = "mythril";
     private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final ForgeTier MYTHRIL = new ForgeTier(2, 800, 7.0F, 2.5F, 20, BlockTags.NEEDS_IRON_TOOL, () -> Ingredient.of(ItemRegistry.mythril_ingot.get()));
 
     public static final CreativeModeTab CREATIVETAB = new CreativeModeTab(MODID) {
         @Override
--- src/main/java/shiroroku/mythril/Registries/ItemRegistry.java
+++ src/main/java/shiroroku/mythril/Registries/ItemRegistry.java
@@ -1,6 +1,6 @@
 package shiroroku.mythril.Registries;
 
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -16,6 +16,12 @@ public class ItemRegistry {
     public static final RegistryObject<Item> mythril_ingot = ITEMS.register("mythril_ingot", basicItem());
     public static final RegistryObject<Item> mythril_nugget = ITEMS.register("mythril_nugget", basicItem());
     public static final RegistryObject<Item> raw_mythril = ITEMS.register("raw_mythril", basicItem());
+
+    public static final RegistryObject<Item> mythril_axe = ITEMS_HANDHELD.register("mythril_axe", () -> new AxeItem(Mythril.MYTHRIL, 6.0F, -3.1F, defaultProp()));
+    public static final RegistryObject<Item> mythril_hoe = ITEMS_HANDHELD.register("mythril_hoe", () -> new HoeItem(Mythril.MYTHRIL, -2, -1.0F, defaultProp()));
+    public static final RegistryObject<Item> mythril_pickaxe = ITEMS_HANDHELD.register("mythril_pickaxe", () -> new PickaxeItem(Mythril.MYTHRIL, 1, -2.8F, defaultProp()));
+    public static final RegistryObject<Item> mythril_shovel = ITEMS_HANDHELD.register("mythril_shovel", () -> new ShovelItem(Mythril.MYTHRIL, 1.5F, -3.0F, defaultProp()));
+    public static final RegistryObject<Item> mythril_sword = ITEMS_HANDHELD.register("mythril_sword", () -> new SwordItem(Mythril.MYTHRIL, 3, -2.4F, defaultProp()));
 
     public static Item.Properties defaultProp() {
         return new Item.Properties().tab(Mythril.CREATIVETAB);
